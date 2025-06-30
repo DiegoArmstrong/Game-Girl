c1b1f3d3dbedbf039f8f9041df5cc50ef3d5b53a
Diego <diegoarmstrong84@gmail.com>
2025-06-29 21:53:53 -0700

    Added initialization of Select Button. Also created functions for reading select button GPIO and joystick ADC channels.

--- game-girl-console/components/gg_joystick/gg_joystick_driver.cpp
+++ game-girl-console/components/gg_joystick/gg_joystick_driver.cpp
@@ -1,23 +1,29 @@
 #include "gg_joystick_driver.h"
-#include "driver/adc.h"
-#include "esp_log.h"
 
 static const char* TAG = "JoystickDriver";
 
-/*
-* @brief   The initializaton functon to start up any necessary perpherals and resources needed
-*            for the Joystck Driver to operate.
-*
-* @retval  ESP_OK      Returned if the initialization was successful.
-* @retval  ESP_FAIL    Returned if the initialization failed.
-*/
+/*!
+ * @brief   The initializaton functon to start up any necessary perpherals and resources needed
+ *            for the Joystck Driver to operate.
+ *
+ * @retval  ESP_OK      Returned if the initialization was successful.
+ * @retval  ESP_FAIL    Returned if the initialization failed.
+ */
 esp_err_t JoystickDriver::init() {
     esp_err_t ret = ESP_OK;
+    gpio_config_t selectButtonConfig = {};
 
-    /* Configure the ADC channel and attenuation. */
-    ret = adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_12);
+    /* Configure the X-axis ADC channel and attenuation. */
+    ret = adc1_config_channel_atten(JOYSTICK_X_AXIS, ADC_ATTEN_DB_12);
     if(ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to configure the ADC1 channel attenuation within JoystickDriver::init().");
+        ESP_LOGE(TAG, "Failed to configure the X-axis channel attenuation within JoystickDriver::init().");
+        goto err;
+    }
+
+    /* Configure the Y-axis ADC channel and attenuation. */
+    ret = adc1_config_channel_atten(JOYSTICK_Y_AXIS, ADC_ATTEN_DB_12);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to configure the Y-axis ADC channel attenuation within JoystickDriver::init().");
         goto err;
     }
 
@@ -28,8 +34,53 @@ esp_err_t JoystickDriver::init() {
         goto err;
     }
 
+    /* Configure the Joystick Select Button. */
+    selectButtonConfig.intr_type = GPIO_INTR_DISABLE;
+    selectButtonConfig.mode = GPIO_MODE_INPUT;
+    selectButtonConfig.pin_bit_mask = 1ULL << JOYSTICK_SELECT_BUTTON;
+    selectButtonConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    selectButtonConfig.pull_up_en = GPIO_PULLUP_ENABLE;
+
+    ret = gpio_config(&selectButtonConfig);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to configure the Joystick Select Button GPIO within JoystickDriver::init().");
+        goto err;
+    }
+
     return ESP_OK;
 
 err:
     return ESP_FAIL;
+}
+
+/*!
+ * @brief   Reads the raw values of the X and Y axis ADC channels.
+ *
+ * @param[out]          xAxis   The X-axis ADC channel raw value.
+ * @param[out]          yAxis   The Y-axis ADC channel raw value.
+ *
+ * @retval  ESP_OK                  Returned if the reading was successful.
+ * @retval  ESP_FAIL                Returned if the reading failed.  
+ */
+esp_err_t JoystickDriver::retrieveRawJoystickValues(uint16_t &xAxis, uint16_t &yAxis) {
+    esp_err_t ret = ESP_OK;
+
+    /* Get the raw readings. */
+    xAxis = adc1_get_raw(JOYSTICK_X_AXIS);
+    yAxis = adc1_get_raw(JOYSTICK_Y_AXIS);
+
+    return ESP_OK;
+}
+
+/*!
+ * @brief   Reads the select button GPIO of the Joystick.
+ *
+ * @retval  ESP_OK    Returned if the reading was successful.
+ */
+esp_err_t retrieveSelectButtonGpioLevel(uint8_t &selectButtonGpioLevel) {
+    esp_err_t ret = ESP_OK;
+
+    selectButtonGpioLevel = gpio_get_level(JOYSTICK_SELECT_BUTTON);
+
+    return ESP_OK;
 }
--- game-girl-console/components/gg_joystick/include/gg_joystick_driver.h
+++ game-girl-console/components/gg_joystick/include/gg_joystick_driver.h
@@ -1,14 +1,42 @@
 #include "esp_err.h"
+#include "driver/adc.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+/* Assigns ADC channels to the Joystick's X and Y axes. */
+#define JOYSTICK_X_AXIS ADC1_CHANNEL_1
+#define JOYSTICK_Y_AXIS ADC1_CHANNEL_2
+
+/* Assigns a GPIO with the Joystick Select Button. */
+#define JOYSTICK_SELECT_BUTTON  GPIO_NUM_14
 
 class JoystickDriver {
 public:
 
-    /*
+    /*!
      * @brief   The initializaton functon to start up any necessary peripherals and resources needed
-                    for the Joystck Driver to operate.
+     *              for the Joystck Driver to operate.
      *
      * @retval  ESP_OK      Returned if the initialization was successful.
      * @retval  ESP_FAIL    Returned if the initialization failed.
      */
     esp_err_t init();
+
+    /*!
+     * @brief   Reads the raw values of the X and Y axis ADC channels.
+     *
+     * @param[out]          xAxis   The X-axis ADC channel raw value.
+     * @param[out]          yAxis   The Y-axis ADC channel raw value.
+     *
+     * @retval  ESP_OK                  Returned if the reading was successful.
+     * @retval  ESP_FAIL                Returned if the reading failed.  
+     */
+    esp_err_t retrieveRawJoystickValues(uint16_t &xAxis, uint16_t &yAxis);
+
+    /*!
+     * @brief   Reads the select button GPIO of the Joystick.
+     *
+     * @retval  ESP_OK    Returned if the reading was successful.
+     */
+    esp_err_t retrieveSelectButtonGpioLevel(uint8_t &selectButtonGpioLevel);
 };
